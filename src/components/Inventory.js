c397b74dfd49e538fce014fc17e6bb5477014343
RoyCrivolotti <roy.crivolotti@southworks.com>
2019-09-17 13:19:25 -0300

    Deleted commented section

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -7,16 +7,6 @@ class Inventory extends React.Component {
 	render() {
 		return (
 			<div className='inventory'>
-				{/* <h2>Inventory</h2>
-			 {Object.keys(this.props.fishes).map(key => (
-				 <EditFishForm
-				 key={key}
-				 index={key}
-				 fish={this.props.fishes[key]}
-				 updateFish={this.props.updateFish}
-				 />
-				 ))} */}
-
 				<h2>Inventory</h2>
 				<EditFishForm
 					fishes={Object.keys(this.props.fishes).map(key => ({
